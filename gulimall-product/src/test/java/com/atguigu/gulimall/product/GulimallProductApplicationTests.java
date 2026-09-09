80e19070edbefae39fdcd285bc63a10b2f05e9b7
liujiaxu <15866718620@163.com>
2026-09-09 18:08:59 +0800

    fix(gulimall):测试依赖修改

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,4 +1,4 @@
-package com.atguigu.gulimall;
+package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
